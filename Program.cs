6b2021a3ce642bbef2eb07e96dc0e6449a670bc0
Guolong Li <Guolong.LiRealtor@gmail.com>
2022-12-30 22:32:44 -0600

    KanyeWest

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Net.WebRequestMethods;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+Console.WriteLine("Hello, World!");
+var kanyeURL = "https://api.kanye.rest";
+var client = new HttpClient();
+var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+Console.WriteLine(kanyeQuote); 
